a698317d077bcd949fdacdf22f65df9a75a7700c
daniel-santos-7 <danielsantos7@outlook.com.br>
2020-05-10 18:44:37 -0300

    cors configurado

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,8 +1,15 @@
 const express = require('express');
+const cors = require('cors');
 const routes = require('./routes');
 const io = require('socket.io');
 
 const app = express();
+
+const corsConfig = { 
+    exposedHeaders:['X-Total-Count']
+};
+
+app.use(cors(corsConfig));
 
 app.use(express.json());
 
